e0c121e0b0754567ba6ce01012a5fade7673ae8e
小蜗 <627054877@qq.com>
2018-02-04 16:13:29 +0800

    2018-02-04
    loading组件, 推荐列表滚动条

--- src/views/recommend/recommend.vue
+++ src/views/recommend/recommend.vue
@@ -1,35 +1,50 @@
 <template>
-  <div class="recommend">
-    <div class="recommend-content">
-      <div class="slider-wrapper" v-if="recommends.length">
-        <slider>
-          <div v-for="(x, index) in recommends" :key="index">
-            <a :href="x.linkUrl">
-              <img :src="x.picUrl" alt="">
-            </a>
-          </div>
-        </slider>
+  <div class="recommend" ref="recommend">
+    <scroll ref="scroll" class="recommend-content" :data="discList">
+      <div>
+        <div v-if="recommends.length" class="slider-wrapper" ref="sliderWrapper">
+          <slider>
+            <div v-for="(item, index) in recommends" :key="index">
+              <a :href="item.linkUrl">
+                <img class="needsclick" @load="loadImage" :src="item.picUrl">
+              </a>
+            </div>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li @click="selectItem(item)" v-for="(item,index) in discList" class="item" :key="index">
+              <div class="icon">
+                <img width="60" height="60" v-lazy="item.imgurl">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-        <li v-for="(x, index) in discList" class="item" :key="index">
-          <div class="icon">
-            <img width="60" height="60" v-lazy="x.imgurl">
-          </div>
-          <div class="text">
-            <h2 class="name" v-html="x.creator.name"></h2>
-            <p class="desc" v-html="x.dissname"></p>
-          </div>
-        </li>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
       </div>
-    </div>
+    </scroll>
+    <router-view></router-view>
   </div>
 </template>
-<script>
-import { getRecommend, getDiscList } from '@/api/recommend.js'
-import { ERR_OK } from '@/api/config.js'
+
+<script type="text/ecmascript-6">
 import Slider from '@/base/slider/slider'
+import Loading from '@/base/loading/loading'
+import Scroll from '@/base/scroll/scroll'
+import { getRecommend, getDiscList } from '@/api/recommend'
+import { playlistMixin } from '@/common/js/mixin'
+import { ERR_OK } from '@/api/config'
+import { mapMutations } from 'vuex'
+
 export default {
+  mixins: [playlistMixin],
   data() {
     return {
       recommends: [],
@@ -38,12 +53,28 @@ export default {
   },
   created() {
     this._getRecommend()
+
     this._getDiscList()
   },
-  components: {
-    Slider
-  },
   methods: {
+    handlePlaylist(playlist) {
+      const bottom = playlist.length > 0 ? '60px' : ''
+
+      this.$refs.recommend.style.bottom = bottom
+      this.$refs.scroll.refresh()
+    },
+    loadImage() {
+      if (!this.checkloaded) {
+        this.checkloaded = true
+        this.$refs.scroll.refresh()
+      }
+    },
+    selectItem(item) {
+      this.$router.push({
+        path: `/recommend/${item.dissid}`
+      })
+      this.setDisc(item)
+    },
     _getRecommend() {
       getRecommend().then(res => {
         if (res.code === ERR_OK) {
@@ -57,11 +88,20 @@ export default {
           this.discList = res.data.list
         }
       })
-    }
+    },
+    ...mapMutations({
+      setDisc: 'SET_DISC'
+    })
+  },
+  components: {
+    Slider,
+    Loading,
+    Scroll
   }
 }
 </script>
-<style lang="stylus" scoped>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
 @import '../../common/stylus/variable';
 
 .recommend {
